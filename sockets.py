f9d895e61b2ad98cd4d64fd5f8db4a6dbe58884f
moxil-shah <shahmv200216@gmail.com>
2022-11-26 14:51:13 -0700

    DONE

--- sockets.py
+++ sockets.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect, jsonify
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -26,6 +26,20 @@ app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
 
+# from https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py
+clients = list()
+
+# from https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+    
+    def put(self, v):
+        self.queue.put_nowait(v)
+        
+    def get(self):
+        return self.queue.get()
+    
 class World:
     def __init__(self):
         self.clear()
@@ -61,26 +75,64 @@ class World:
 
 myWorld = World()        
 
+# from https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py
+def send_all(msg):
+    for client in clients:
+        client.put( msg )
+
+# from https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py
+def send_all_json(obj):
+    send_all( json.dumps(obj) )
+
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    for client in clients:
+        client.put(json.dumps({entity:data}))
 
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html", code=302)
 
+# from https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            msg = ws.receive()
+            if (msg is not None):  
+                packet = json.loads(msg)
+                for entity in packet:
+                    myWorld.set(entity, packet[entity])
+                send_all_json( packet )
+            else:
+                break
+    except:
+        '''Done'''
 
+# from https://github.com/abramhindle/WebSocketsExamples/blob/master/chat.py
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn(read_ws, ws, client)
+    print("Subscribing")
+    try:
+        while True:
+            # block here
+            msg = client.get()
+            print("Got a message!")
+            ws.send(msg)
+    except Exception as e: # WebSocketError as e
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
     return None
 
 
@@ -99,23 +151,31 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    dataToUpdate = flask_post_json()
+    if myWorld.get(entity):
+        for each in dataToUpdate:
+            myWorld.update(entity, each, dataToUpdate[each])
+    else:
+        myWorld.set(entity, dataToUpdate)
+
+    return jsonify(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
-    '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    '''This is the GET version of the entity interface, return a representation of the entity''' 
+    return jsonify(myWorld.get(entity))
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return jsonify(myWorld.world()) 
 
 
 
